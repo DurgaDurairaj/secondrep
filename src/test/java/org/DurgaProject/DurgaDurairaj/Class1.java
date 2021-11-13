592f2a863187307ad5cb944c4dc7567bd963bee9
DurgaDurairaj <durgadurairaj23@gmail.com>
2021-11-13 12:17:31 +0530

    secondpush

--- src/test/java/org/DurgaProject/DurgaDurairaj/Class1.java
+++ src/test/java/org/DurgaProject/DurgaDurairaj/Class1.java
@@ -1,5 +1,17 @@
 package org.DurgaProject.DurgaDurairaj;
 
 public class Class1 {
+	private void methodA() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodB() {
+		// TODO Auto-generated method stub
+
+	}
+	private void methodC() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
